f2591f1d84cd25e2828ccc2929009a06376164a8
Anum-Rehman <anumr32@gmail.com>
2023-01-13 16:33:45 +0500

    config updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+import nextEnv from 'next-env';
+const prod = process.env.NODE_ENV === 'production'
 
-module.exports = nextConfig
+
+const withNextEnv = nextEnv();
+
+export default withNextEnv({
+    async redirects() {
+        return [
+            {
+                source: '/list',
+                destination: '/',
+                permanent: true
+            },
+        ];
+    },
+    env: {
+        ROOT_DIR: process.env.ROOT_DIR,
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')]
+    },
+    images: {
+        domains: ['res.cloudinary.com', 'cdn.filestackcontent.com'],
+        minimumCacheTTL: 60,
+    },
+    reactStrictMode: true,
+    compiler: {
+        styledComponents: true,
+    }
+});
