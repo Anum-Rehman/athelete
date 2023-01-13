d83d5322822b89a38cdf6da90366879701231c69
Anum-Rehman <anumr32@gmail.com>
2023-01-13 16:36:40 +0500

    config changed

--- next.config.js
+++ next.config.js
@@ -1,31 +1,6 @@
-import nextEnv from 'next-env';
-const prod = process.env.NODE_ENV === 'production'
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-
-const withNextEnv = nextEnv();
-
-export default withNextEnv({
-    async redirects() {
-        return [
-            {
-                source: '/list',
-                destination: '/',
-                permanent: true
-            },
-        ];
-    },
-    env: {
-        ROOT_DIR: process.env.ROOT_DIR,
-    },
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')]
-    },
-    images: {
-        domains: ['res.cloudinary.com', 'cdn.filestackcontent.com'],
-        minimumCacheTTL: 60,
-    },
-    reactStrictMode: true,
-    compiler: {
-        styledComponents: true,
-    }
-});
+module.exports = nextConfig
